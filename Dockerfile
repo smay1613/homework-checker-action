d2e11f7f0236c307c09e5b33034a4ea13dcc2545
smay1613 <dimaafa0@gmail.com>
2019-11-09 00:29:27 +0200

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,17 @@
 FROM ubuntu:latest
 
-LABEL com.github.actions.name="clang-tidy check"
-LABEL com.github.actions.description="Lint your code with clang-tidy in parallel to your builds"
+LABEL com.github.actions.name="homework-checker"
+LABEL com.github.actions.description="Action for automatic students homework checking"
 LABEL com.github.actions.icon="code"
 LABEL com.github.actions.color="gray-dark"
 
-LABEL repository="https://github.com/smay1613/clang-tidy-action"
+LABEL repository="https://github.com/smay1613/homework-checker-action"
 LABEL maintainer="smay1613 <dimaafa0@gmail.com>"
 
 WORKDIR /build
 RUN apt-get update
-RUN apt-get -qq -y install curl
-RUN apt-get -y -qq install clang-tidy cmake jq clang cppcheck
+RUN apt-get -qq -y install curl jq
 
-ADD runchecks.sh /entrypoint.sh
+ADD runchecks.sh /runchecks.sh
 COPY . .
-CMD ["bash", "/entrypoint.sh"]
+CMD ["bash", "/runchecks.sh"]
--- runchecks.sh
+++ runchecks.sh
@@ -1,54 +1,36 @@
 #!/bin/bash
 
-if [[ -z "$GITHUB_TOKEN" ]]; then
-	echo "The GITHUB_TOKEN is required."
-	exit 1
+echo $GITHUB_EVENT_PATH
+cat $GITHUB_EVENT_PATH
+
+COLUMN_URL=$(jq -r '.project_card.column_url' "$GITHUB_EVENT_PATH")/files
+echo "Project link = $PROJECT_URL"
+
+curl $COLUMN_URL > column.json
+
+cat column.json
+
+COLUMN_NAME=`jq -r '.name' column.json`
+
+echo "Column name: $COLUMN_NAME"
+
+if ["$COLUMN_NAME" = "Ready to test"]; then
+    	echo "No updates. $COLUMN_NAME"
+	exit 0
 fi
 
-FILES_LINK=`jq -r '.pull_request._links.self.href' "$GITHUB_EVENT_PATH"`/files
-echo "Files = $FILES_LINK"
-
-curl $FILES_LINK > files.json
-FILES_URLS_STRING=`jq -r '.[].raw_url' files.json`
-
-readarray -t URLS <<<"$FILES_URLS_STRING"
-
-echo "File names: $URLS"
-
-mkdir files
-cd files
-for i in "${URLS[@]}"
-do
-   echo "Downloading $i"
-   curl -LOk --remote-name $i 
-done
-
-echo "Files downloaded!"
 echo "Performing checkup:"
-clang-tidy --version
-clang-tidy *.cpp -checks=boost-*,bugprone-*,performance-*,readability-*,portability-*,modernize-*,clang-analyzer-cplusplus-*,clang-analyzer-*,cppcoreguidelines-* > clang-tidy-report.txt
 
-cppcheck --enable=all --std=c++11 --language=c++ --output-file=cppcheck-report.txt *
+#OUTPUT=$'**CLANG WARNINGS**:\n'
+#OUTPUT+=$'\n```\n'
+#OUTPUT+="$PAYLOAD_CLANG"
+#OUTPUT+=$'\n```\n'
 
-PAYLOAD_CLANG=`cat clang-tidy-report.txt`
-PAYLOAD_CPPCHECK=`cat cppcheck-report.txt`
-COMMENTS_URL=$(cat $GITHUB_EVENT_PATH | jq -r .pull_request.comments_url)
-  
-echo $COMMENTS_URL
-echo "Clang errors:"
-echo $PAYLOAD_CLANG
-echo "Cppcheck errors:"
-echo $PAYLOAD_CPPCHECK
-OUTPUT=$'**CLANG WARNINGS**:\n'
-OUTPUT+=$'\n```\n'
-OUTPUT+="$PAYLOAD_CLANG"
-OUTPUT+=$'\n```\n'
+#OUTPUT+=$'\n**CPPCHECK WARNINGS**:\n'
+#OUTPUT+=$'\n```\n'
+#OUTPUT+="$PAYLOAD_CPPCHECK"
+#OUTPUT+=$'\n```\n' 
 
-OUTPUT+=$'\n**CPPCHECK WARNINGS**:\n'
-OUTPUT+=$'\n```\n'
-OUTPUT+="$PAYLOAD_CPPCHECK"
-OUTPUT+=$'\n```\n' 
+#PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
 
-PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
-
-curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/vnd.github.VERSION.text+json" --data "$PAYLOAD" "$COMMENTS_URL"
+#curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/vnd.github.VERSION.text+json" --data "$PAYLOAD" "$COMMENTS_URL"
