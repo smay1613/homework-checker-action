10feeac6baac4e335f2e971adb961743bc8e555f
smay1613 <dimaafa0@gmail.com>
2019-11-18 21:31:44 +0200

    Removes redudant things from script

--- runchecks.sh
+++ runchecks.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-echo $GITHUB_EVENT_PATH
 cat $GITHUB_EVENT_PATH
 
-COLUMN_URL=$(jq -r '.project_card.column_url' "$GITHUB_EVENT_PATH")/files
-echo "Project link = $PROJECT_URL"
+COLUMN_URL=$(jq -r '.project_card.column_url' "$GITHUB_EVENT_PATH")
+echo "Column link = $PROJECT_URL"
 
 curl $COLUMN_URL > column.json
 
@@ -20,17 +19,3 @@ if ["$COLUMN_NAME" = "Ready to test"]; then
 fi
 
 echo "Performing checkup:"
-
-#OUTPUT=$'**CLANG WARNINGS**:\n'
-#OUTPUT+=$'\n```\n'
-#OUTPUT+="$PAYLOAD_CLANG"
-#OUTPUT+=$'\n```\n'
-
-#OUTPUT+=$'\n**CPPCHECK WARNINGS**:\n'
-#OUTPUT+=$'\n```\n'
-#OUTPUT+="$PAYLOAD_CPPCHECK"
-#OUTPUT+=$'\n```\n' 
-
-#PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
-
-#curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/vnd.github.VERSION.text+json" --data "$PAYLOAD" "$COMMENTS_URL"
