ee6dca5bfb341f88e064b28a4fbc439dcadbccdd
smay1613 <dimaafa0@gmail.com>
2019-11-18 21:47:39 +0200

    Fixes columns request

--- runchecks.sh
+++ runchecks.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "The GITHUB_TOKEN is required."
+	exit 1
+fi
 
 cat $GITHUB_EVENT_PATH
 
 COLUMN_URL=$(jq -r '.project_card.column_url' "$GITHUB_EVENT_PATH")
-echo "Column link = $PROJECT_URL"
+echo "Column link = $COLUMN_URL"
 
-curl $COLUMN_URL > column.json
+curl -s -S -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.inertia-preview+json" $COLUMN_URL > column.json
 
+echo "Column info:"
 cat column.json
 
-COLUMN_NAME=`jq -r '.name' column.json`
+COLUMN_NAME=$(jq -r '.name' column.json)
 
 echo "Column name: $COLUMN_NAME"
 
-if ["$COLUMN_NAME" = "Ready to test"]; then
+if [[ "$COLUMN_NAME" != "Ready to test" ]]; then
     	echo "No updates. $COLUMN_NAME"
 	exit 0
 fi
