7965419ef33dcc776d4bf3fbaf431ce64fbbd696
Dmytro Afanasiev <DAfanasiev@luxoft.com>
2019-11-30 03:25:05 +0200

    Makes debug output conditional

--- runchecks.sh
+++ runchecks.sh
@@ -4,19 +4,24 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
 	exit 1
 fi
 
-cat $GITHUB_EVENT_PATH
+function debug() {
+  if [[ -n "$DEBUG" ]]; then
+     echo "[DEBUG] $1"
+  fi
+}
+
+debug $(cat $GITHUB_EVENT_PATH)
 
 COLUMN_URL=$(jq -r '.project_card.column_url' "$GITHUB_EVENT_PATH")
-echo "Column link = $COLUMN_URL"
+debug $(echo "Column link = $COLUMN_URL")
 
 curl -s -S -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.inertia-preview+json" $COLUMN_URL > column.json
 
-echo "Column info:"
-cat column.json
+debug $(echo "Column info:" && cat column.json)
 
 COLUMN_NAME=$(jq -r '.name' column.json)
 
-echo "Column name: $COLUMN_NAME"
+debug $(echo "Column name: $COLUMN_NAME")
 
 if [[ "$COLUMN_NAME" != "Ready to test" ]]; then
     	echo "No updates. $COLUMN_NAME"
@@ -27,34 +32,40 @@ echo "Performing checkup:"
 PULLS_ROOT_URL=$(jq -r '.repository.pulls_url' "$GITHUB_EVENT_PATH")
 PULLS_ROOT_URL=${PULLS_ROOT_URL%\{/number\}}
 
-echo "Pulls url: $PULLS_ROOT_URL"
+debug $(echo "Pulls url: $PULLS_ROOT_URL")
 curl -s -S -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github+json $PULLS?state=open" $PULLS_ROOT_URL > pulls_data.json
 
-echo "Pulls info:"
-cat pulls_data.json
+debug $(echo "Pulls info:" && cat pulls_data.json)
 
 CARD_NOTE=$(jq -r '.project_card.note' "$GITHUB_EVENT_PATH")
 TASK_NUMBER=$(echo $CARD_NOTE | grep -oPe "(task.|lab.)\K\d+")
 CARD_ID=$(jq -r '.project_card.id' "$GITHUB_EVENT_PATH")
 
-echo -e "Task number = $TASK_NUMBER;\nCard note: $CARD_NOTE;\nCard id: $CARD_ID\n"
+debug $(echo -e "Task number = $TASK_NUMBER;\nCard note: $CARD_NOTE;\nCard id: $CARD_ID\n")
 
 jq -r ".[] | select (.title | contains(\"$CARD_ID\")) | .url" pulls_data.json > pulls
 
-echo "Pulls to be checked:"
-cat pulls
+debug $(echo "Pulls to be checked:" && cat pulls)
 
 REPO_URL=$(jq -r '.repository.url' "$GITHUB_EVENT_PATH")
-echo "Repo URL: $REPO_URL"
+
+debug $(echo "Repo URL: $REPO_URL")
+
+TESTS_URL="https://github.com/smay1613/Luxoft-Training-Center-CPP-003-Tests/archive/master.zip"
+curl -u $ACCESS_TOKEN -s -L $TESTS_URL --output tests.zip
+unzip tests.zip -d tests
+TESTS_DIR="$PWD/tests"
+
+debug $(ls tests)
 
 function processPullRequest() {
   PR_ID=$(echo $1 | grep -oP -e '\d.$')
-  echo "Processing pull request with id $PR_ID"
+  debug $(echo "Processing pull request with id $PR_ID")
   ARCHIVE_URL="$REPO_URL/zipball/pull/$PR_ID/head"
-  curl -s -L $ARCHIVE_URL --output "$PR_ID.zip" && echo "Downloaded successfully" || (echo "Cannot download!" && exit 1)
-  unzip $PR_ID.zip > $PR_ID
-  cd $PR_ID && ls | xargs cd
-  ls
+  curl -s -L $ARCHIVE_URL --output "$PR_ID.zip" && debug $(echo "Downloaded successfully") || (echo "Cannot download!" && exit 1)
+  unzip $PR_ID.zip -d $PR_ID
+  cd $(ls)
+  debug $(ls)
 }
 
 cat pulls | while read pr_link; do
